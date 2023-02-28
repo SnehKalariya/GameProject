ffcd5100425ba9d21c03f21c163fe27931ded163
SnehKalariya <snehkalariya9583@gmail.com>
2023-02-28 12:25:30 +0530

    abc

--- GameProject/ViewController.swift
+++ GameProject/ViewController.swift
@@ -1,19 +1,51 @@
-//
-//  ViewController.swift
-//  GameProject
-//
-//  Created by Sneh kalariya on 19/02/23.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var playButtonOutlet: UIButton!
+    @IBOutlet weak var loadingLabelOutlet: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    var time = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        progressBar.progress = 0.0
+    }
+    
+    func progress(){
+        var value : Float = 0.0
+        self.progressBar.progress = value
+        time = Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true, block: { (time) in
+            value += 0.02
+            self.progressBar.progress = value
+            if self.progressBar.progress == 1.0{
+                self.progressBar.progress = 0.0
+                self.showAlert(title: "Click to play & game start")
+                self.naviget()
+                self.time.invalidate()
+            }
+        })
+    }
+    
+    func showAlert(title:String){
+        let alert = UIAlertController(title: "Error", message: title, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Play", style: .default,handler: { _ in
+            self.naviget()
+        }))
+        
+        present(alert,animated: true)
+    }
+    
+    func naviget(){
+        let naviget = storyboard?.instantiateViewController(withIdentifier: "ViewController2") as! ViewController2
+        navigationController?.pushViewController(naviget, animated: true)
     }
 
-
+    @IBAction func playButtonAction(_ sender: UIButton) {
+        progress()
+    }
+    
 }
 
---
+++ GameProject/ViewController3.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController3.swift
+//  GameProject
+//
+//  Created by Sneh kalariya on 22/02/23.
+//
+
+import UIKit
+
+class ViewController3: UIViewController {
+
+    var frequncy = 0.1
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
